f9b16ca273297d65a3456bdd4224c072d952c9fd
KarinaBugiel0 <karina.bugiel0@gmail.com>
2024-05-25 18:24:35 +0200

    Finished Task3

--- ImpYML.py
+++ ImpYML.py
@@ -6,12 +6,3 @@ class ImpYML(ImpExp):
     def __init__(self, filename):
         super().__init__(filename)
 
-    def RunOperation(self):
-        try:
-            with open(self.filename, 'r') as file:
-                new_data = yaml.safe_load(file)
-                
-            self.FetchData(new_data)
-            self.ImportSuccessful()
-        except Exception as e:
-            raise Exception(f"{Const.GET_ERROR('ERROR_IMPYML')} {str(e)}")
